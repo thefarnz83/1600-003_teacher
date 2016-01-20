658bf0b87948830b2e0ae26f2ef53a02e6a42a35
Michael Farnsworth <thefarnz83@gmail.com>
2016-01-20 09:22:11 -0700

    Adding if Statement stuff

---
+++ 1600-003_teacher/Assets/Scripts/ifStatements.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour 
+{
+	//This is the test of my new scripts in my branch
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
