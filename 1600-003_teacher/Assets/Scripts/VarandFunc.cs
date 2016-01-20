86546d2e6e61885fa3fb94a4b491bd0ebcdd4b51
Michael Farnsworth <thefarnz83@gmail.com>
2016-01-20 09:15:31 -0700

    Adding in Conventions and Syntax Scene

---
+++ 1600-003_teacher/Assets/Scripts/VarandFunc.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarandFunc : MonoBehaviour 
+{
+	int myInt = 2000;
+	// Use this for initialization
+	void Start () 
+	{
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+	int MultiplyByTwo (int Number)
+	{
+		int ret;
+		ret = Number * 2;
+		return ret;
+	}
+		
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ 1600-003_teacher/Assets/Scripts/VarandFuncPublic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarandFuncPublic : MonoBehaviour 
+{
+	public int myInt = 1000;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = AddOne (myInt);
+		Debug.Log (myInt);
+	}
+	int AddOne (int Number)
+	{
+		int ret;
+		ret = Number + 1000;
+		return ret;
+	}
+}
