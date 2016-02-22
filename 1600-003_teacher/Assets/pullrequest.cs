6bddaac013f58f86c3ec42bb623326c6cdddc9a1
Michael Farnsworth <thefarnz83@gmail.com>
2016-02-22 09:06:21 -0700

    pull request test script and scene

---
+++ 1600-003_teacher/Assets/pullrequest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pullrequest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("This is a test. It does nothing");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
